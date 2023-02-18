a01ef7c3e8aed76596058ebba2c58c56d7bd7334
Olivia Lee <leesol@10-18-129-177.dynapool.wireless.nyu.edu>
2023-02-18 12:32:58 -0500

    get and post

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # creating an instance of Flask class
 # needed so that Flask knows where to look for resources such as templates and static files
 app = Flask(__name__)
 
 # route() decorator to tell Flask what URL should trigger our function
-@app.route("/")
+@app.route("/", methods = ['GET', 'POST'])
 # function returns message we want to display in the user's browser
-def hello_world():
-    return render_template("index.html")
+def index_page():
+    if request.method == 'GET':
+        return render_template("index.html")
+
+    if request.method == 'POST':
+        data = request.form
+        print(data+'\n')
+        return render_template("index.html")
 
 
 
